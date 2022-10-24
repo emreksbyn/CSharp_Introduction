7b05df3429dff9c242457e5fd6c1fef97e2715b7
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-10-24 22:00:47 +0300

    Main Method explained

---
+++ 101.What_Is_Main/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _101.What_Is_Main
+{
+    // Uygulamalarda Program.cs dosyasi : Baslangic kodlarinin bulundugu, yani uygulamanin ayaga kalkabilmesi ile ilgili kodlari bulunduran dosyadir.
+    internal class Program
+    {
+        /*
+         *  Main fonksiyonu :
+         *  1- Sadece 1 tane Main fonksiyonu vardir.
+         *  2- Uygulama ayaga kaltiginda isletim sistemiyle iletisim kuracak metottur.
+         */
+        static void Main(string[] args)
+        {
+            // " dotnet run emre kisaboyun " --> Developer PowerShell den dosyanin bulundugu dizinde bu kodlari yazip calistirirsak
+            // Once Hello World! altinda da emre onun altindada kisaboyun yazar.
+            // Isletim sisteminden uygulamaya bu sekilde deger gondermis oluruz.
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(args[0]);
+            Console.WriteLine(args[1]);
+        }
+    }
+}
+
+#region Top - Level Statements Property
+
+//// Top - Level Statements Ozelligi :
+//// Microservices yapilanmasinda kodun gelisimi acisindan hiz kazandirici bir nitelige sahiptir.
+
+//using System;
+
+
+//// C# 9.0 dan sonra bu sekilde kullanima olanak tanindi. --> .Net 6.0 dan sonra yukaridaki using e de gerek kalmamistir.
+//Console.WriteLine("Hello World!");
+//Console.WriteLine(args[0]);
+//Console.WriteLine(args[1]); 
+#endregion
