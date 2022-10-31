ecf70404beafae26b1db5bd6f6e1917c515ded2e
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-10-31 17:29:00 +0300

    object type, boxing, unboxing, cast operator, var keyword, dynamic keyword added.

---
+++ 103.ObjectType/Keywords.cs
@@ -0,0 +1,53 @@
+﻿namespace _103.ObjectType
+{
+    public class Keywords
+    {
+        // Compile suresinde / Development asamasinda / derleme asamasinda
+        public void VarKeyword()
+        {
+            // Tutulacak degerin turune uygun bir degisken tanimlayabilmek icin kullanilir.
+            // var --> Compiler tarafindan kendisine atanana degerin turu olur.
+            // var bir tur DEGILDIR.
+
+            var age = 27;
+            // burda var int turunu almistir.
+
+            // Genellikle degiskenlerin turlerini yazmaya usendigimiz icin kullaniriz. Aslında ;
+            // Farkli diller arasinda desteklenmeyen turdeki verileri karsilamak icin olusturulmus ortak bir keyword tur.
+
+            // Turunu bilebildigimiz verilerin degisken turlerini var ile compiler a yaptirmak ufakta olsa bir maliyettir. Eger karsilayacagimiz turu biliyorsak o tur ile karsilamaliyiz.
+
+            //var name;  --> hata alir
+            // var ile degisken tanimlayacaksak degerini tanimlarken vermemiz gerekir.
+            var name = "Emre";
+            //name = 67;  --> Sonradan degeri degisecekse ilk degerin turunde degisebilir !!
+            name = "Ali";
+
+            // var bir keyword'tur objcet ise bir turdur. var ile tutulan deger dogrudan kendi turunden islemlerde kullanilir. object de ise unboxing islemi gerekir.
+
+        }
+
+        // Uygulama derlenip calistirildiginda / ayaga kalktiginda / runtime
+        public void DynamicKeyword()
+        {
+            // Uzaktan gelen veriler var keywordu ile karsilanamaz. Cunku tanimlandiklari esnada deger isterler. 
+            // Bu ihtiyacimizi dynamic keyword u ile karsiliyoruz.
+
+            var yas = 27; // var --> int olmustur.
+            yas.ToString(); // yas in ToString() metotu olmasaydı compile aninda hata alirdik.
+
+            dynamic age = 27; // dynamic hala dynamic tir.
+            age.ToString(); // age in ToString() metot u varsa runtime da onu cagiracaktir. Yoksa patlatir.
+
+            // dynamic keyword u runtime da turu belirleyecektir fakat kararli davranmayacaktir.
+            dynamic name = "Emre";
+            Console.WriteLine(name.GetType()); // dynamic {string}
+            name = 67;
+            Console.WriteLine(name.GetType()); // dynamic {int}
+
+            // runtime da degerini karsilayacagi icin compile aninda deger vermesekte patlatmaz.
+            dynamic number; // --> Runtime da turu dynamic {object} olur. Degeri de null olur.
+            Console.WriteLine();
+        }
+    }
+}
--- 103.ObjectType/Program.cs
+++ 103.ObjectType/Program.cs
@@ -1,1 +1,53 @@
-﻿Console.WriteLine();
+﻿using _103.ObjectType;
+
+Console.WriteLine();
+
+#region What is object ?
+// Tum turleri karsilayabilen bir TUR dur.
+// Tum turler varsayilan olarak object ten turerler.
+// "object" referans turlu bir degiskendir. Deger turlu degiskenleri de karsilayabilir.
+// object degiskenler veriyi RAM da object turde tutarlar. Ama verinin oz turunude icerisinde saklarlar.
+// Yani object icerisinde veri kendi oz turunde tutulur. Bu durum object icerisindeki veriyi kendi turunde tekrar elde edebilmemizi saglar.
+#endregion
+
+#region Boxing
+
+// object turderki bir degiskene herhangi bir turdeki degeri atamak Boxing islemidir.
+object age = 27;
+object name = "Emre";
+object isActive = true;
+object firtLetter = 'a';
+// Burada age degeri kendi oz turunde saklanir. Fakat bu age degiskeni artik 28 degerini object turunde getirecektir.
+// Dikkat etmemiz gereken yer boxing yapilan deger uzerinde turune ozgu islemler yapilamaz. Yani sayisal bir deger olan 27 uzerinde matematiksel islemler yapamayiz.
+
+#endregion
+
+#region Cast Operator
+
+// Boxing edilmis bir veriyi kendi turunde elde etmemizi saglayan bit operatordur.
+// ( ) --> Cast operatoru
+// (int)object 
+int a = 5;
+object b = a; // Boxing
+int c = (int)b; // Cast operatoru ile b objesinin degeri c ye int olarak atanmistir.
+
+#endregion
+
+#region UnBoxing
+
+// Boxing edilmis bir veriyi kendi oz turune dondurmeyi saglamaktir.
+//Console.WriteLine(age * 5);// --> hata alir
+
+int _age = (int)age; // UnBoxing
+Console.WriteLine(_age * 5); // Artik int turunun ozelliklerini kullanabilirim.
+
+// UnBoxing esnasinda, Boxing edilmis verinin orijinal turu ne ise o ture unboxing edilmelidir.
+string yas = (string)age;
+// Uygulama ayaga kalmadan hata vermez.
+// Fakat Compiler edilirken Runtime da bu yukaridaki satir bizi patlatir !!
+
+#endregion
+
+Keywords keywords = new();
+keywords.VarKeyword();
+keywords.DynamicKeyword();
